c7031b2ea2c487aabc9a6e4bf75d1d40edbb7450
Chris Rodriguez <github@chrisrodriguez.me>
2016-03-11 08:16:28 -0500

    JShint fix

--- common/lib/xmodule/xmodule/js/src/video/09_video_caption.js
+++ common/lib/xmodule/xmodule/js/src/video/09_video_caption.js
@@ -112,7 +112,7 @@
 
                 var template = [
                     '<div class="subtitles" role="region" aria-label="',
-                        /* jshint maxlen:300 */
+                        /* jshint maxlen:350 */
                         gettext('Video transcript: activating an item in this group will spool the video to the corresponding time point.'),
                         '" id="transcript-' + this.state.id + '">',
                         '<a href="#transcript-end-' + this.state.id + '"',
